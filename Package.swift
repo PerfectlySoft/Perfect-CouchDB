1ea619bbfb40ff0b3703ce853e055de7a4a37dae
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:55:31 -0400

    update versions

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ let package = Package(
     name: "PerfectCouchDB",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2),
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
     ]
 )
