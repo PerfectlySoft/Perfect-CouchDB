4750c24360e2a2b81e2e3c8c058127f82bdbac76
Jonathan Guthrie <jono@perfect.org>
2017-10-03 14:59:16 -0400

    update for perfect 3/swift 4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-11-22 22:17:55 +0000
+// Date: 2017-10-03 18:56:15 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectCouchDB",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
-    ]
+	name: "PerfectCouchDB",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/PerfectLib.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
+	]
 )
